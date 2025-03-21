adcafcb2cc96b118ce43fe6b2b84f9949e180885
Gabriel Nunes <gabrielmiranda.eng@gmail.com>
2025-03-21 17:36:15 -0300

    psc-lista-01-2025/envio_parcial_lista_03 feat(Calc2Grau): criacao de algoritmo que verifica e calcula equacoes de segundo grau

---
+++ src/Lista_03/Calc2grau.java
@@ -0,0 +1,70 @@
+package Lista_03;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calc2grau {
+  public static void main(String[] args) {
+    // Variaveis
+    int switchVer;
+    Double a, b, c, delta, raiz1, raiz2;
+    int verificar;
+
+    // Entrada do usuario
+    try (Scanner scanner = new Scanner(System.in)) {
+      do {
+        System.out.println("Digite os coeficientes da sua equação de 2º grau:\n");
+        while (true) {
+          try {
+            System.out.println("Coeficiente (a): ");
+            a = scanner.nextDouble();
+            System.out.println("Coeficiente (b): ");
+            b = scanner.nextDouble();
+            System.out.println("Coeficiente (c): ");
+            c = scanner.nextDouble();
+            break;
+
+          } catch (InputMismatchException notDouble) {
+            System.out.println("Entrada inválida, tente novamente...");
+            scanner.next();
+
+          }
+        }
+
+        // Verificacao dos coeficientes
+        if (a == 0 && b == 0) {
+          System.err.println("Coeficientes informados incorretamente.\n" +
+              "\n==============================\n");
+          verificar = 1;
+        } else {
+          verificar = 0;
+        }
+      } while (verificar == 1);
+
+      // Calculo Discriminate Delta
+      delta = ((b * b) - 4 * a * c);
+      // Verificacao da equacao
+      if (a == 0 && b != 0) {
+        System.err.println("Esta é uma equação de primeiro grau." +
+            "\nPorém, seu discriminante Delta é igual a: " + delta);
+      } else if (delta < 0) {
+        System.err.println("Esta equação não possui raízes reais." +
+            "\nPorém, seu discriminante Delta é igual a: " + delta);
+      } else if (delta == 0) {
+        raiz1 = ((-b + (Math.sqrt(delta))) / (2 * a));
+        raiz2 = ((-b - (Math.sqrt(delta))) / (2 * a));
+        System.out.println("Esta equação possui duas raizes iguais." +
+            "\nDiscriminante Delta é igual a: " + delta);
+        System.out.printf("A primeira raiz é: %.2f", raiz1);
+        System.out.printf("\nA segunda raiz é: %.2f", raiz2);
+      } else {
+        raiz1 = ((-b + (Math.sqrt(delta))) / (2 * a));
+        raiz2 = ((-b - (Math.sqrt(delta))) / (2 * a));
+        System.out.println("Esta equação possui duas raízes diferentes." +
+            "\nDiscriminante Delta é igual a: " + delta);
+        System.out.printf("A primeira raiz é: %.2f", raiz1);
+        System.out.printf("\nA segunda raiz é: %.2f", raiz2);
+      }
+    }
+  }
+}
