e907f4cb01d596eb56cf03ccf7e7c3615b869a40
Gabriel Nunes <gabrielmiranda.eng@gmail.com>
2025-03-27 18:58:02 -0300

    psc-lista-01-2025/envio_parcial_lista_03 fix(Calc2Grau): correcao de erro do nome da classe; feat(SCSMs): adicao de prompt para cancelamento da compra antes do pagamento; docs(SCSMs): adicao de anotacoes para cada etapa do codigo;

--- src/Lista_03/SCSMs.java
+++ src/Lista_03/SCSMs.java
@@ -10,11 +10,11 @@ public class SCSMs {
 
     public static void main(String[] args) {
         Locale country = Locale.getDefault();
-        String compra, notasFormat, valorCompraFormat;
+        String compra, notasFormat, valorCompraFormat, cancelarS;
         int precoSalg = 5, precoDoce = 8, precoSuco = 3, precoRefri = 10;
         int qtdSalg = 0, qtdDoce = 0, qtdSuco = 0, qtdRefri = 0;
         int valorCompra, valorPago, valorTroco, pedido;
-        boolean finalCompra;
+        boolean finalCompra, cancelarPedido;
         int[] notas = { 50, 20, 10, 5, 2, 1 };
         int[] qtdNotas = new int[notas.length];
 
@@ -25,111 +25,127 @@ public class SCSMs {
         String precoRefriFormat = currencyFormat.format(precoRefri);
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("\nBem-vindo à Máquina de Satisfação de Desejos Espontâneos! S.C.S.M.\n"
-                    + "Aqui você pode comprar salgados, doces, sucos e refrigerantes.\n\n"
-                    + "=====================================\n\n"
-                    + "Aqui estão os preços dos produtos:\n"
-                    + "[1] Salgado: " + precoSalgFormat + "\n"
-                    + "[2] Doce: " + precoDoceFormat + "\n"
-                    + "[3] Suco: " + precoSucoFormat + "\n"
-                    + "[4] Refrigerante: " + precoRefriFormat + "\n\n"
-                    + "=====================================\n");
-
+            // Menu inicial da SCSMs
             do {
-                while (true) {
-                    try {
-                        System.out.println("Digite qual produto que deseja comprar [1 - 4]: ");
-                        pedido = scanner.nextInt();
-                        if (pedido < 1 || pedido > 4) {
+                System.out.println("\nBem-vindo à Máquina de Satisfação de Desejos Espontâneos! S.C.S.M.\n"
+                        + "Aqui você pode comprar salgados, doces, sucos e refrigerantes.\n\n"
+                        + "=====================================\n\n"
+                        + "Aqui estão os preços dos produtos:\n"
+                        + "[1] Salgado: " + precoSalgFormat + "\n"
+                        + "[2] Doce: " + precoDoceFormat + "\n"
+                        + "[3] Suco: " + precoSucoFormat + "\n"
+                        + "[4] Refrigerante: " + precoRefriFormat + "\n\n"
+                        + "=====================================\n");
+
+                // Escolha do produto
+                do {
+                    while (true) {
+                        try {
+                            System.out.println("Digite qual produto que deseja comprar [1 - 4]: ");
+                            pedido = scanner.nextInt();
+                            if (pedido < 1 || pedido > 4) {
+                                System.out.println(
+                                        "Opção inválida...");
+                                continue;
+                            }
+                            break;
+                        } catch (InputMismatchException notInt) {
                             System.out.println(
-                                    "Opção inválida...");
-                            continue;
+                                    "Entrada inválida... ");
+                            scanner.next();
                         }
-                        break;
-                    } catch (InputMismatchException notInt) {
-                        System.out.println(
-                                "Entrada inválida... ");
-                        scanner.next();
+
                     }
 
+                    switch (pedido) {
+                        case 1:
+                            while (true) {
+                                try {
+                                    System.out.println("Digite a quantidade total de salgados que deseja comprar: ");
+                                    qtdSalg = scanner.nextInt();
+                                    break;
+                                } catch (InputMismatchException notInt) {
+                                    System.out.println(
+                                            "Entrada inválida... ");
+                                    scanner.next();
+                                }
+                            }
+                            break;
+                        case 2:
+                            while (true) {
+                                try {
+                                    System.out.println("Digite a quantidade total de doces que deseja comprar: ");
+                                    qtdDoce = scanner.nextInt();
+                                    break;
+                                } catch (InputMismatchException notInt) {
+                                    System.out.println(
+                                            "Entrada inválida... ");
+                                    scanner.next();
+                                }
+                            }
+                            break;
+                        case 3:
+                            while (true) {
+                                try {
+                                    System.out.println("Digite a quantidade total de sucos que deseja comprar: ");
+                                    qtdSuco = scanner.nextInt();
+                                    break;
+                                } catch (InputMismatchException notInt) {
+                                    System.out.println(
+                                            "Entrada inválida... ");
+                                    scanner.next();
+                                }
+                            }
+                            break;
+                        case 4:
+                            while (true) {
+                                try {
+                                    System.out
+                                            .println("Digite a quantidade total de refrigerantes que deseja comprar: ");
+                                    qtdRefri = scanner.nextInt();
+                                    break;
+                                } catch (InputMismatchException notInt) {
+                                    System.out.println(
+                                            "Entrada inválida... ");
+                                    scanner.next();
+                                }
+                            }
+                            break;
+                        default:
+                            System.out.println("Opção inválida...");
+                    }
+
+                    System.out.println("Deseja comprar mais alguma coisa? [S / N]");
+                    compra = scanner.next();
+                    finalCompra = compra.equalsIgnoreCase("N");
+
+                } while (finalCompra == false);
+
+                // Calculo do total da compra
+                valorCompra = (qtdSalg * precoSalg) + (qtdDoce * precoDoce) + (qtdSuco * precoSuco)
+                        + (qtdRefri * precoRefri);
+                valorCompraFormat = currencyFormat.format(valorCompra);
+
+                System.out.println("\n=====================================\n\n"
+                        + "O valor total da sua compra é: " + valorCompraFormat);
+
+                System.out.println("Esta máquina aceita apenas as notas de: \n");
+                for (int i = 0; i < notas.length; i++) {
+                    notasFormat = currencyFormat.format(notas[i]);
+                    System.out.println(notasFormat);
                 }
 
-                switch (pedido) {
-                    case 1:
-                        while (true) {
-                            try {
-                                System.out.println("Digite a quantidade de salgados que deseja comprar: ");
-                                qtdSalg = scanner.nextInt();
-                                break;
-                            } catch (InputMismatchException notInt) {
-                                System.out.println(
-                                        "Entrada inválida... ");
-                                scanner.next();
-                            }
-                        }
-                        break;
-                    case 2:
-                        while (true) {
-                            try {
-                                System.out.println("Digite a quantidade de doces que deseja comprar: ");
-                                qtdDoce = scanner.nextInt();
-                                break;
-                            } catch (InputMismatchException notInt) {
-                                System.out.println(
-                                        "Entrada inválida... ");
-                                scanner.next();
-                            }
-                        }
-                        break;
-                    case 3:
-                        while (true) {
-                            try {
-                                System.out.println("Digite a quantidade de sucos que deseja comprar: ");
-                                qtdSuco = scanner.nextInt();
-                                break;
-                            } catch (InputMismatchException notInt) {
-                                System.out.println(
-                                        "Entrada inválida... ");
-                                scanner.next();
-                            }
-                        }
-                        break;
-                    case 4:
-                        while (true) {
-                            try {
-                                System.out.println("Digite a quantidade de refrigerantes que deseja comprar: ");
-                                qtdRefri = scanner.nextInt();
-                                break;
-                            } catch (InputMismatchException notInt) {
-                                System.out.println(
-                                        "Entrada inválida... ");
-                                scanner.next();
-                            }
-                        }
-                        break;
-                    default:
-                        System.out.println("Opção inválida...");
+                System.out.println("\nDeseja continuar sua compra [S / N]");
+                cancelarS = scanner.next();
+                cancelarPedido = cancelarS.equalsIgnoreCase("N");
+
+                if (cancelarPedido == true) {
+                    System.out.println("\nSeu pedido foi cancelado..." +
+                            "\n\n=====================================\\n\\n");
                 }
+            } while (cancelarPedido == true);
 
-                System.out.println("Deseja comprar mais alguma coisa? [S / N]");
-                compra = scanner.next();
-                finalCompra = compra.equalsIgnoreCase("N");
-
-            } while (finalCompra == false);
-
-            valorCompra = (qtdSalg * precoSalg) + (qtdDoce * precoDoce) + (qtdSuco * precoSuco)
-                    + (qtdRefri * precoRefri);
-            valorCompraFormat = currencyFormat.format(valorCompra);
-
-            System.out.println("\n=====================================\n\n"
-                    + "O valor total da sua compra é: " + valorCompraFormat);
-
-            System.out.println("Esta máquina aceita apenas as notas de: \n");
-            for (int i = 0; i < notas.length; i++) {
-                notasFormat = currencyFormat.format(notas[i]);
-                System.out.println(notasFormat);
-            }
-
+            // Pagamento e calculo de troco
             do {
                 while (true) {
                     try {
